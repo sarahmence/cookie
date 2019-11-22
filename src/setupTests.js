082d70e4896b531253d4473aff3fca7a7682deca
ahdavis <ahdavis13@gmail.com>
2019-11-22 14:03:07 -0600

    Added Enzyme setup script

---
+++ src/setupTests.js
@@ -0,0 +1,30 @@
+/*
+ * setupTests.js
+ * Initializes Enzyme testing framework
+ * Created on 11/22/2019
+ * Created by Andrew Davis
+ *
+ * Copyright (C) 2019  Andrew Davis
+ *
+ * This program is free software: you can redistribute it and/or modify   
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//imports
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+//configure Enzyme
+configure({ adapter: new Adapter() });
+
+//end of file
