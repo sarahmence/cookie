49e09e44f94d8f043263ce67f0f1ee81e632f047
ahdavis <ahdavis13@gmail.com>
2019-11-22 14:10:35 -0600

    Added WindowProps interface

---
+++ src/props/WindowProps.ts
@@ -0,0 +1,32 @@
+/*
+ * WindowProps.ts
+ * Defines properties of the Window component
+ * Created on 11/22/2019
+ * Created by Andrew Davis
+ *
+ * Copyright (C) 2019  Andrew Davis
+ *
+ * This program is free software: you can redistribute it and/or modify   
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//no imports
+
+/**
+ * The properties of the [[Window]] component
+ */
+export interface WindowProps {
+	//TODO: Add properties
+}
+
+//end of file
