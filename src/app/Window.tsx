e42213d294a754001049f56deab0a355578b24ee
ahdavis <ahdavis13@gmail.com>
2019-11-22 14:08:23 -0600

    Added Window component

---
+++ src/app/Window.tsx
@@ -0,0 +1,56 @@
+/*
+ * Window.tsx
+ * Defines the window component for Cookie
+ * Created on 11/22/2019
+ * Created by Andrew Davis
+ *
+ * Copyright (C) 2019  Andrew Davis
+ *
+ * This program is free software: you can redistribute it and/or modify   
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//imports
+import * as React from 'react';
+import { WindowProps } from '../props/WindowProps';
+import { WindowState } from '../state/WindowState';
+
+/**
+ * The window of the app
+ */
+export class Window extends React.Component<WindowProps, WindowState> {
+	//no fields
+
+	/**
+	 * Constructs a new `Window` component
+	 *
+	 * @param props The properties of the `Window`
+	 */
+	constructor(props: WindowProps) {
+		super(props); //call the superclass constructor
+	}
+
+	/**
+	 * Renders the `Window`
+	 *
+	 * @returns A React DOM representing the `Window`
+	 */
+	public render(): React.ReactNode {
+		//TODO: Render proper components
+		return (
+			<h1>Hello, World!</h1>
+		);
+	}
+}
+
+//end of file
