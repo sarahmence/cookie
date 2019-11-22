64dbda0e45dd66e6300fae73556d026baab33b17
ahdavis <ahdavis13@gmail.com>
2019-11-22 14:00:56 -0600

    Added renderer entry point

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,36 @@
+/*
+ * index.tsx
+ * Renderer entry point for Cookie
+ * Created on 11/22/2019
+ * Created by Andrew Davis
+ *
+ * Copyright (C) 2019  Andrew Davis
+ *
+ * This program is free software: you can redistribute it and/or modify   
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//imports
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './app/App';
+import * as serviceWorker from './util/serviceWorker';
 
+//render the app component
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//and start the service worker
+serviceWorker.register();
+
+//end of file
