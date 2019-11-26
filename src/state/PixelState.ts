fb992cd6ded1f83f088baf8303cfe425134fe49a
ahdavis <ahdavis13@gmail.com>
2019-11-26 11:44:33 -0600

    Added PixelState interface

---
+++ src/state/PixelState.ts
@@ -0,0 +1,32 @@
+/*
+ * PixelState.ts
+ * Defines state fields for the Pixel component
+ * Created on 11/26/2019
+ * Created by Andrew Davis
+ *
+ * Copyright (C) 2019  Andrew Davis
+ *
+ * This program is free software: you can redistribute it and/or modify   
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//no imports
+
+/**
+ * The state of a [[Pixel]]
+ */
+export interface PixelState {
+	//no code needed
+}
+
+//end of file
