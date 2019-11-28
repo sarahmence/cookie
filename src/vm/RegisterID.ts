21f9bb463b19af566db96c6c99844b6ac98ba587
ahdavis <ahdavis13@gmail.com>
2019-11-28 11:32:19 -0600

    Added RegisterID enum

---
+++ src/vm/RegisterID.ts
@@ -0,0 +1,47 @@
+/*
+ * RegisterID.ts
+ * Enumerates IDs of CPU registers
+ * Created on 11/28/2019
+ * Created by Andrew Davis
+ *
+ * Copyright (C) 2019  Andrew Davis
+ *
+ * This program is free software: you can redistribute it and/or modify   
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//no imports
+
+/**
+ * The IDs of CPU registers
+ */
+export enum RegisterID {
+	V0 = 0x0,
+	V1 = 0x1,
+	V2 = 0x2,
+	V3 = 0x3,
+	V4 = 0x4,
+	V5 = 0x5,
+	V6 = 0x6,
+	V7 = 0x7,
+	V8 = 0x8,
+	V9 = 0x9,
+	VA = 0xA,
+	VB = 0xB,
+	VC = 0xC,
+	VD = 0xD,
+	VE = 0xE,
+	VF = 0xF
+}
+
+//end of file
