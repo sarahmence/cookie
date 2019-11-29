11ab7ad17e744d88084eec7a9cd666e7b3104aab
ahdavis <ahdavis13@gmail.com>
2019-11-28 18:43:13 -0600

    Added Executor type

---
+++ src/vm/Executor.ts
@@ -0,0 +1,30 @@
+/*
+ * Executor.ts
+ * Defines a type that defines an executor function
+ * Created on 11/28/2019
+ * Created by Andrew Davis
+ *
+ * Copyright (C) 2019  Andrew Davis
+ *
+ * This program is free software: you can redistribute it and/or modify   
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//no imports
+
+/**
+ * A function that executes a given opcode
+ */
+export type Executor = (opcode: number) => Promise<boolean>;
+
+//end of file
